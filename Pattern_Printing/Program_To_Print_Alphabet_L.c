67a97939131b30dcb08ed46b93dca6261abff563
Shrutipatil23 <patilshrutis7597@gmail.com>
2022-11-16 22:19:19 +0530

    .

---
+++ Pattern_Printing/Program_To_Print_Alphabet_L.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int r=0,c=0,Cnt=0;
+
+    printf("\nENTER THE ROW AND COLUMN COUNT : ");
+    scanf("%d",&Cnt);
+
+    printf("\nPATTERN FOR I IS AS FOLLOWS : ");
+
+    for(r=1;r<=Cnt;r++)
+    {
+        printf("\n\t\t\t");
+        for (c=1;c<=Cnt;c++)
+        {
+            if(c==1 || r==Cnt)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");  
+    }
+    return 0;
+}
