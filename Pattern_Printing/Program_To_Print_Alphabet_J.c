c3670621ca29d2fcf7b692c0d55d7c0e982c0a3c
Shrutipatil23 <patilshrutis7597@gmail.com>
2022-11-23 12:52:22 +0530

    Added Pattern J

---
+++ Pattern_Printing/Program_To_Print_Alphabet_J.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int r=0 , c=0 , Cnt=0;
+    printf("\nENTER THE ROW AND COLUMN COUNT : ");
+    scanf("%d",&Cnt);
+
+    printf("\nTHE PATTERN FOR J IS AS FOLLOWS : ");
+    for(r=1;r<=Cnt;r++)
+    {
+        printf("\n\t\t\t");
+        for(c=1;c<=Cnt;c++)
+        {
+            if(r==1 || c==(Cnt+1)/2 || r==Cnt && c<=(Cnt+1)/2)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
